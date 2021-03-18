4e75a29e0d861d1459a0ed30b2ca48ef06324a2c
Ibnathism <1605106@ugrad.cse.buet.ac.bd>
2021-03-19 01:23:52 +0600

    refactor

--- Open-GL/functions.h
+++ Open-GL/functions.h
@@ -28,11 +28,7 @@ void clear() {
 
 
 void drawAxes() {
-
-
-    glBegin(GL_LINES);
-
-    {
+    glBegin(GL_LINES);{
         glVertex3f(100, 0, 0);
         glVertex3f(-100, 0, 0);
 
@@ -41,20 +37,12 @@ void drawAxes() {
 
         glVertex3f(0, 0, 100);
         glVertex3f(0, 0, -100);
-    }
-
-    glEnd();
-
-
+    }glEnd();
 }
 
 void drawGrid() {
-
     int i;
-
-    glBegin(GL_LINES);
-
-    {
+    glBegin(GL_LINES);{
         for (i = -8; i <= 8; i++) {
 
             if (i == 0)
@@ -68,16 +56,11 @@ void drawGrid() {
             glVertex3f(-90, i * 10, 0);
             glVertex3f(90, i * 10, 0);
         }
-    }
-
-    glEnd();
-
-
+    }glEnd();
 }
 
 void drawSquare(double a)
 {
-    //glColor3f(1.0,0.0,0.0);
     glBegin(GL_QUADS);{
         glVertex3f( a, a,2);
         glVertex3f( a,-a,2);
@@ -104,31 +87,22 @@ void draw2DRectangle(Point topLeft, Point bottomLeft, Point topRight, Point bott
 }
 
 
-void drawCircle(double radius,int segments)
-{
+void drawCircle(double radius,int segments) {
     int i;
     Point points[100];
-    //glColor3f(0.7,0.7,0.7);
-    //generate points
-    for(i=0;i<=segments;i++)
-    {
+    for(i=0;i<=segments;i++) {
         points[i].x=radius*cos(((double)i/(double)segments)*2*pi);
         points[i].y=radius*sin(((double)i/(double)segments)*2*pi);
     }
-    //draw segments using generated points
-    for(i=0;i<segments;i++)
-    {
-        glBegin(GL_LINES);
-        {
+    for(i=0;i<segments;i++) {
+        glBegin(GL_LINES);{
             glVertex3f(points[i].x,points[i].y,0);
             glVertex3f(points[i+1].x,points[i+1].y,0);
-        }
-        glEnd();
+        }glEnd();
     }
 }
 
-void draw2DCircle(double centerX, double centerY, double radius,int segments)
-{
+void draw2DCircle(double centerX, double centerY, double radius,int segments) {
     int i;
     Point points[100];
     for(i=0;i<=segments;i++)
@@ -140,77 +114,58 @@ void draw2DCircle(double centerX, double centerY, double radius,int segments)
     }
     for(i=0;i<segments;i++)
     {
-        glBegin(GL_LINES);
-        {
+        glBegin(GL_LINES);{
             glVertex3f(points[i].x,points[i].y,0);
             glVertex3f(points[i+1].x,points[i+1].y,0);
-        }
-        glEnd();
+        }glEnd();
     }
 }
 
 
 
-void drawCone(double radius,double height,int segments)
-{
+void drawCone(double radius,double height,int segments){
     int i;
     double shade;
     Point points[100];
-    //generate points
-    for(i=0;i<=segments;i++)
-    {
+    for(i=0;i<=segments;i++) {
         points[i].x=radius*cos(((double)i/(double)segments)*2*pi);
         points[i].y=radius*sin(((double)i/(double)segments)*2*pi);
     }
-    //draw triangles using generated points
-    for(i=0;i<segments;i++)
-    {
-        //create shading effect
+    for(i=0;i<segments;i++) {
         if(i<segments/2)shade=2*(double)i/(double)segments;
         else shade=2*(1.0-(double)i/(double)segments);
         glColor3f(shade,shade,shade);
 
-        glBegin(GL_TRIANGLES);
-        {
+        glBegin(GL_TRIANGLES);{
             glVertex3f(0,0,height);
             glVertex3f(points[i].x,points[i].y,0);
             glVertex3f(points[i+1].x,points[i+1].y,0);
-        }
-        glEnd();
+        }glEnd();
     }
 }
 
-
-void drawSphere(double radius,int slices,int stacks)
-{
+void drawSphere(double radius,int slices,int stacks) {
     Point points[100][100];
     int i,j;
     double h,r;
-    //generate points
-    for(i=0;i<=stacks;i++)
-    {
+    for(i=0;i<=stacks;i++) {
         h=radius*sin(((double)i/(double)stacks)*(pi/2));
         r=radius*cos(((double)i/(double)stacks)*(pi/2));
-        for(j=0;j<=slices;j++)
-        {
+        for(j=0;j<=slices;j++) {
             points[i][j].x=r*cos(((double)j/(double)slices)*2*pi);
             points[i][j].y=r*sin(((double)j/(double)slices)*2*pi);
             points[i][j].z=h;
         }
     }
-    //draw quads using generated points
-    for(i=0;i<stacks;i++)
-    {
+    for(i=0;i<stacks;i++) {
         glColor3f((double)i/(double)stacks,(double)i/(double)stacks,(double)i/(double)stacks);
-        for(j=0;j<slices;j++)
-        {
+        for(j=0;j<slices;j++) {
             glBegin(GL_QUADS);{
-                //upper hemisphere
                 glVertex3f(points[i][j].x,points[i][j].y,points[i][j].z);
                 glVertex3f(points[i][j+1].x,points[i][j+1].y,points[i][j+1].z);
                 glVertex3f(points[i+1][j+1].x,points[i+1][j+1].y,points[i+1][j+1].z);
                 glVertex3f(points[i+1][j].x,points[i+1][j].y,points[i+1][j].z);
-                //lower hemisphere
+
                 glVertex3f(points[i][j].x,points[i][j].y,-points[i][j].z);
                 glVertex3f(points[i][j+1].x,points[i][j+1].y,-points[i][j+1].z);
                 glVertex3f(points[i+1][j+1].x,points[i+1][j+1].y,-points[i+1][j+1].z);
@@ -219,34 +174,6 @@ void drawSphere(double radius,int slices,int stacks)
         }
     }
 }
-
-//void drawSS()
-//{
-//    glColor3f(1,0,0);
-//    drawSquare(20);
-//
-//    glRotatef(angle,0,0,1);
-//    glTranslatef(110,0,0);
-//    glRotatef(2*angle,0,0,1);
-//    glColor3f(0,1,0);
-//    drawSquare(15);
-//
-//    glPushMatrix();
-//    {
-//        glRotatef(angle,0,0,1);
-//        glTranslatef(60,0,0);
-//        glRotatef(2*angle,0,0,1);
-//        glColor3f(0,0,1);
-//        drawSquare(10);
-//    }
-//    glPopMatrix();
-//
-//    glRotatef(3*angle,0,0,1);
-//    glTranslatef(40,0,0);
-//    glRotatef(4*angle,0,0,1);
-//    glColor3f(1,1,0);
-//    drawSquare(5);
-//}
 
 double getRandom(double a, double b) {
     std::random_device randomDevice;
