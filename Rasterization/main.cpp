fd0f7a0c80ae20b855ca313a63ba1328db7ab230
Ibnathism <1605106@ugrad.cse.buet.ac.bd>
2021-05-18 22:40:17 +0600

    refactor point class

--- Rasterization/main.cpp
+++ Rasterization/main.cpp
@@ -1,6 +1,79 @@
 #include <iostream>
+class Point
+{
+public:
+    double x,y,z;
 
+    Point() {
+        x=0;
+        y=0;
+        z=0;
+    }
+
+    Point(double x, double y, double z) : x(x), y(y), z(z) {}
+
+    Point operator-() const {
+        return {-this->x, -this->y, -this->z};
+    }
+
+    double dotMultiplication(Point &temp) const {
+        double dot = this->x*temp.x + this->y*temp.y + this->z*temp.z;
+        return dot;
+    }
+
+    Point operator+(Point &temp) const {
+        double ansX = this->x + temp.x;
+        double ansY = this->y + temp.y;
+        double ansZ = this->z + temp.z;
+        return {ansX, ansY, ansZ};
+    }
+
+    Point operator-(Point &temp) const {
+        double ansX = this->x - temp.x;
+        double ansY = this->y - temp.y;
+        double ansZ = this->z - temp.z;
+        return {ansX, ansY, ansZ};
+    }
+
+    Point operator*(double scalingConstant) const {
+        if (scalingConstant==0) return {0, 0,0};
+        double ansX = scalingConstant * this->x;
+        double ansY = scalingConstant * this->y;
+        double ansZ = scalingConstant * this->z;
+        return {ansX, ansY, ansZ};
+    }
+
+    Point operator/(double divisionConstant) const {
+        double ansX = this->x / divisionConstant;
+        double ansY = this->y / divisionConstant;
+        double ansZ = this->z / divisionConstant;
+        return {ansX, ansY, ansZ};
+    }
+
+    Point operator*(Point &temp) const {
+        double ansX = this->x * temp.x;
+        double ansY = this->y * temp.y;
+        double ansZ = this->z * temp.z;
+        return {ansX, ansY, ansZ};
+    }
+
+    Point crossMultiplication(Point &temp) const {
+        double ansX = this->y*temp.z - this->z*temp.y;
+        double ansY = this->z*temp.x - this->x*temp.z;
+        double ansZ = this->x*temp.y - this->y*temp.x;
+        return {ansX, ansY, ansZ};
+    }
+
+    void print() const {
+        std::cout << '(' << this->x <<", " << this->y << ", " << this->z << ')' << std::endl;
+    }
+};
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    Point P1(1.0, 1.0, 1.0);
+    Point P2(2.0, 2.0, 2.0);
+    Point P3 = P1 * P2;
+    P3.print();
     return 0;
 }
